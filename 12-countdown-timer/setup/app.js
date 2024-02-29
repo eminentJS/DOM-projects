c4a38eeb28ce6b6f09b8242ad7ff5a642964c777
yusuf abdulsalam <eminentjs@gmail.com>
2024-02-28 23:07:38 -0500

    calculate remaining time

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -27,8 +27,7 @@ const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
 // the date format below is year, month, day, hours, minutes, seconds
-let futureDate = new Date(2020, 3, 25, 11, 30, 0);
-console.log(futureDate);
+let futureDate = new Date(2024, 4, 25, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -44,3 +43,52 @@ const weekday = weekdays[futureDate.getDay()];
 console.log(weekday);
 
 giveaway.textContent = `giveaway ends of ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+// console.log(futureTime);
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  // console.log(today);
+  const t = futureDate - today;
+  // 1s = 1000ms
+  // 1m =60s
+  // 1hr = 60min
+  // 1d = 24hr
+
+  // value in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  // console.log(oneDay);
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set value array
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  // if the time has expired
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired<h4>`;
+  }
+}
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
